e4d098e1780d2b5ad6a5e4478b0078da2bfc37c0
akibmir90 <maaqib90@gmail.com>
2025-04-29 10:26:56 +0530

    Latest data fetched

--- web/config.py
+++ web/config.py
@@ -3,8 +3,8 @@ from keras.metrics import MeanSquaredError
 
 class Config:
     # Model and Data Paths
-    MODEL_PATH = r'D:\Git Projects\Price_forecasting_project\Agricultural-Price-Intelligence\models\Narwal\Delicious\lstm_Delicious.h5'
-    DATA_PATH = r'D:\Git Projects\Price_forecasting_project\Agricultural-Price-Intelligence\data\raw\processed\Narwal\Delicious_dataset.csv'
+    MODEL_PATH = r'D:\Git Projects\Price_forecasting_project\Agricultural-Price-Intelligence\models\Shopian\lstm_Kullu Delicious_grade_A.h5'
+    DATA_PATH = r'D:\Git Projects\Price_forecasting_project\Agricultural-Price-Intelligence\data\raw\processed\Shopian\Kullu Delicious_A_dataset.csv'
 
     # Custom objects for loading model
     CUSTOM_OBJECTS = {'mse': MeanSquaredError()}
